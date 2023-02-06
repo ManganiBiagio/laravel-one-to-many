c2e4899b1eda5b28132d02962175a6f851f03285
ManganiBiagio <biagiomangani@live.it>
2023-02-06 17:58:49 +0100

    creation table and relation  Type-Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,4 +11,7 @@ class Project extends Model
     protected $fillable=[
         "name","description","cover_img","github_link"
     ];
+    public function types(){
+        return $this->hasMany(Type::class);
+    } 
 }
